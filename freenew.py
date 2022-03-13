9a540aac837d5869d8fc6978799194cfba440129
rsihi <hunterpckgh@gmail.com>
2022-03-13 15:51:26 +0530

    colorbar

--- freenew.py
+++ freenew.py
@@ -3,13 +3,9 @@ import matplotlib.pyplot as pl
 x = [2,10,12,15,19]
 y = [105,134,156,167,178]
 
-pl.scatter(x,y, color = '#88c999')
+color_for_dot = [10,20,50,70,90]
 
-x = [1,5,13,9,17,11]
-y = [100,140,120,155,175,133]
+pl.scatter(x,y, c=color_for_dot, cmap="viridis")
 
-pl.scatter(x,y)
-
-
-
+pl.colorbar()
 pl.show()
