60782088957adb048e89bd5bda380df9829d4d08
rsihi <hunterpckgh@gmail.com>
2022-03-13 15:43:47 +0530

    scatter

---
+++ freenew.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as pl
+
+x = [2,10,12,15,19]
+y = [105,134,156,167,178]
+
+pl.scatter(x,y, color = '#88c999')
+
+x = [1,5,13,9,17,11]
+y = [100,140,120,155,175,133]
+
+pl.scatter(x,y)
+
+
+
+pl.show()
